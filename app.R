a55ee35f48045c1f54e484177ae575ade3af4a89
Matthew Young <myoung7@uw.edu>
2019-03-11 18:52:39 -0700

    add app.R

---
+++ app.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(shiny)
+library(ggplot2)
+library(rsconnect)
+library(tidyr)
+
+my_ui <- fluidPage(
+  
+  
+)
+
+my_server <- function(input, output){
+  
+}
+
+shinyApp(ui = my_ui, server = my_server)
