2632b70245ab99f649cad075f2a707b3e5e2ffeb
Matthew Young <myoung7@uw.edu>
2019-03-12 00:35:43 -0700

    test

--- app.R
+++ app.R
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 
 
-source("ui.R")
-source("server.R")
-=======
 source("my_ui.R")
 source("my_server.R")
->>>>>>> 0acf548598664d672fa3ddbde2163f5a459638f9
 
 shinyApp(ui = my_ui, server = my_server)
